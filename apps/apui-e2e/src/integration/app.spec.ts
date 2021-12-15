abad4d2b38a63a0f2402d3be9d96245c20fd4a83
initdc <initd@outlook.com>
2021-12-15 13:08:30 +0800

    [dev.12.15] fix testing

--- apps/apui-e2e/src/integration/app.spec.ts
+++ apps/apui-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('apui', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome apui');
+    getGreeting().contains('Welcome @initdc/apui');
   });
 });
